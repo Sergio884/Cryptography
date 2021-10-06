652df83809ccc70846608da62f4ef783adde88b2
sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2021-10-06 12:18:03 -0500

    Esta perro el Angel

--- CBC.py
+++ CBC.py
@@ -61,11 +61,12 @@ def cbcDecifrado(ct,iv,segmento,k,alphabet):
 
     return pt
 
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6"]
 read500 = open("500.txt","r")
 pt = read500.readline()
 read500.close()
-iv = "iii"
+#iv = "iii"
+iv = "aaa"
 k = (1,1,1)
 
 create500CT = open("500CT.txt","w")
