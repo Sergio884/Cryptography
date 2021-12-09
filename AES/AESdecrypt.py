0df475a7c0ccf3c6e254c7967b2f00deab68b5d1
sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2021-12-08 23:45:50 -0600

    Image encryption with AES using CBC operation mode

--- AES/AESdecrypt.py
+++ AES/AESdecrypt.py
@@ -1,16 +1,18 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 
-key = b'rfghuioftedgydrf'
-cFile = open("Cipher","rb")
+kFile = open("Key","rb")
+key = kFile.read()
+kFile.close()
+
+cFile = open("img_Cipher.jpg","rb")
 iv = cFile.read(len(key))
 ciphertext = cFile.read()
 cFile.close()
 
 cipher = AES.new(key, AES.MODE_CBC, iv)
 plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
-print(plaintext)
 
-pFile = open("Plaintext_OUT.txt","wb")
+pFile = open("img_Decrypted.jpg","wb")
 pFile.write(plaintext)
 pFile.close()
--- AES/AESencrypt.py
+++ AES/AESencrypt.py
@@ -1,17 +1,22 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes
 
-key = b'rfghuioftedgydrf'
+key = get_random_bytes(16)
+
+kFile = open("Key","wb")
+kFile.write(key)
+kFile.close()
+
 cipher = AES.new(key, AES.MODE_CBC)
 
-#plaintext = b'Pvto el que lo lea'
-pFile = open("Plaintext_IN","rb")
+pFile = open("img_Original.jpg","rb")
 plaintext = pFile.read()
 pFile.close
 
 ciphertext = cipher.encrypt(pad(plaintext,AES.block_size))
 
-cFile = open("Cipher","wb")
+cFile = open("img_Cipher.jpg","wb")
 cFile.write(cipher.iv)
 cFile.write(ciphertext)
 cFile.close()
